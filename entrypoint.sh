4245824a9dd1735ce166a1eff67fec70ad16b75e
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-01-11 19:10:15 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 git clone "$selfrepository"
 git clone "$datarepository"
+#giving permissions
+chmod +x /gh-repo-bulk-randomizer/nginx.sh
+chmod +x /gh-repo-bulk-randomizer/randomizer.sh
 #doing without supervisor
 log="dumb.log"
 match="nothing to match run nginx run "
 
 echo "nothing to match run nginx run "; bash /gh-repo-bulk-randomizer/nginx.sh > "$log" 2>&1 &
-pid=$!
+
 
 while sleep 60
 do
