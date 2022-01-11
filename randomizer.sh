82b9835d5cec86b4e06e5e8ba7192d8930b3ed6e
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-01-11 18:12:56 +0530

    Update randomizer.sh

--- randomizer.sh
+++ randomizer.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-echo "$gh_token" > /token.txt
 
 #authorize
+echo "$gh_token" > /token.txt
 gh auth login --with-token < /token.txt
-#authorized
+#authorized using token
 
+#repository list update
+wget "$repolist_in_url" -O /.ignore
+cat /.ignore |sed '/#/d' >repolist.txt
+#repository list updated to repolist.txt
+
+#
