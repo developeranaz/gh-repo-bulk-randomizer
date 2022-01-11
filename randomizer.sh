85430c7114b2d2bfb35a002a11b7237be3aaafe9
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-01-11 23:54:16 +0530

    Update randomizer.sh

--- randomizer.sh
+++ randomizer.sh
@@ -9,7 +9,7 @@ gh auth login --with-token < /token.txt
 wget "$repolist_in_url" -O /.ignore
 cat /.ignore |sed '/#/d' >/repolist.txt
 #repository list updated to repolist.txt
-cat repolist.txt |sed 's/^/gh repo clone /g' |sed 's/$/; cd \//g' >s1
-cat /repolist.txt |sed 's|https://github.com/|prefix180|g' |sed 's/\//\nsuperprefix180/g' |grep 'superprefix180' |sed 's/superprefix180//g' >reponame.txt
+cat repolist.txt |sed 's/^/gh repo clone /g' |sed 's/$/; cd \//g' >/s1
+cat /repolist.txt |sed 's|https://github.com/|prefix180|g' |sed 's/\//\nsuperprefix180/g' |grep 'superprefix180' |sed 's/superprefix180//g' >/reponame.txt
 paste -d'\0' /s1 /reponame.txt
 #not yet coml*ted
