aab4be4f2616ab4bbe655d039c51eba18721a565
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-01-12 12:57:23 +0530

    Update randomizer.sh

--- randomizer.sh
+++ randomizer.sh
@@ -11,7 +11,7 @@ cat /.ignore |sed '/#/d' >/repolist.txt
 #repository list updated to repolist.txt
 cat repolist.txt |sed 's/^/gh repo clone /g' |sed 's/$/; cd \//g' >/s1
 cat /repolist.txt |sed 's|https://github.com/|prefix180|g' |sed 's/\//\nsuperprefix180/g' |grep 'superprefix180' |sed 's/superprefix180//g' >/reponame.txt
-paste -d'\0' /s1 /reponame.txt |sed 's/$/; gh repo rename "developeranaz-$REPONAME$RANDOM" -y; #99999999/g' 
+paste -d'\0' /s1 /reponame.txt |sed 's/$/; gh repo rename "developeranaz-$REPONAME$RANDOM" -y; /g' 
 #env name
 cat reponame.txt |sed 's/^/REPONAME="/g' |sed 's/$/"; /g'
 ## end line +(above )not compl*d
