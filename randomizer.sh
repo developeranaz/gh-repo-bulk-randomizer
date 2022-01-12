18974c7b2043de94aabcf6414976d90c6acb8120
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-01-12 14:29:15 +0530

    Update randomizer.sh

--- randomizer.sh
+++ randomizer.sh
@@ -12,7 +12,7 @@ cat /.ignore |sed '/#/d' >/repolist.txt
 cat /repolist.txt |sed 's|https://github.com/|prefix180|g' |sed 's/\//\nsuperprefix180/g' |grep 'superprefix180' |sed 's/superprefix180//g' >/reponame.txt
 cat /reponame.txt |sed 's/^/REPONAME="/g' |sed 's/$/"; /g' >env1
 #repo new name env2
-cat ./reponame.txt|sed 's/$/ignoreme170/g' |sed 's/-n-/\n/g' |grep 'developeranaz' |sed 's/^/REPONEWNAME="/g' |sed 's/$/-n-$(randomhash)"; /g' envnewname
+cat /reponame.txt|sed 's/$/ignoreme170/g' |sed 's/-n-/\n/g' |grep 'developeranaz' |sed 's/^/REPONEWNAME="/g' |sed 's/$/-n-$(randomhash)"; /g' /envnewname
 
 cat /repolist.txt |sed 's/^/gh repo clone /g' |sed 's/$/; cd \//g' >/s1
 paste -d'\0' /envnewname /env1 /s1 /reponame.txt |sed 's/$/; gh repo rename "$REPONEWNAME" -y; bash \/script.sh; git init; git add .; git commit -m "$date"; git push; cd \//g'
