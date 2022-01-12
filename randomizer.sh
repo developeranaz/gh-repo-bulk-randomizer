a0cb2243eb59f1e6dd0b3c00ca4b36a43de709aa
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-01-12 20:52:38 +0530

    Update randomizer.sh

--- randomizer.sh
+++ randomizer.sh
@@ -17,9 +17,12 @@ cat /reponame.txt|sed 's/$/ignoreme170/g' |sed 's/-n-/\n/g' |grep 'developeranaz
 #linkname env3
 cat /reponame.txt|sed 's/$/ignoreme170/g' |sed 's/-n-/\n/g' |grep 'developeranaz' >repoid
 #dottxt
-cat /repoid |sed 's/$/.txt/g' >repolinktxt
+cat /repoid |sed 's/$/.txt/g' >/repolinktxt
 #to data repo 
 # echo "https://$prefix/$newnames" >/datarepo/$datareponame/$repolinktxt; √push
+cat /envnewname | sed 's|^|echo "$PREFIXURL/|g' |sed 's/$/" >\/datarepo\/$datareponame\//g' >/push1
+paste -d'\0' /push1 /repolinktxt >savedottxt
+#paste push1
 
 cat /repolist.txt |sed 's/^/gh repo clone /g' |sed 's/$/; cd \//g' >/s1
 paste -d'\0' /envnewname /env1 /s1 /reponame.txt |sed 's/$/; gh repo rename "$REPONEWNAME" -y; bash \/script.sh; git init; git add .; git commit -m "$date"; git push; cd \//g'
