f4bb1c3845485b4a0df336ab8c0018d6ba23ab50
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-01-12 13:12:40 +0530

    Update randomizer.sh

--- randomizer.sh
+++ randomizer.sh
@@ -9,10 +9,9 @@ gh auth login --with-token < /token.txt
 wget "$repolist_in_url" -O /.ignore
 cat /.ignore |sed '/#/d' >/repolist.txt
 #repository list updated to repolist.txt
-cat repolist.txt |sed 's/^/gh repo clone /g' |sed 's/$/; cd \//g' >/s1
+cat /reponame.txt |sed 's/^/REPONAME="/g' |sed 's/$/"; /g' >env1
+cat /repolist.txt |sed 's/^/gh repo clone /g' |sed 's/$/; cd \//g' >/s1
 cat /repolist.txt |sed 's|https://github.com/|prefix180|g' |sed 's/\//\nsuperprefix180/g' |grep 'superprefix180' |sed 's/superprefix180//g' >/reponame.txt
-paste -d'\0' /s1 /reponame.txt |sed 's/$/; gh repo rename "developeranaz-$REPONAME$RANDOM" -y; /g' 
-#env name
-cat reponame.txt |sed 's/^/REPONAME="/g' |sed 's/$/"; /g'
+paste -d'\0' /env1 /s1 /reponame.txt |sed 's/$/; gh repo rename "developeranaz-$REPONAME$RANDOM" -y; bash \/script.sh; git init; git add .; git commit -m "$date"; git push; cd \//g'
 ## end line +(above )not compl*d
 #not yet coml*ted
