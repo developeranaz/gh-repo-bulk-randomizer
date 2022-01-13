f476999ab6db0d727dcd092d1dbaaefb4a6483f0
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-01-13 18:14:38 +0530

    Update randomizer.sh

--- randomizer.sh
+++ randomizer.sh
@@ -9,25 +9,37 @@ gh auth login --with-token < /token.txt
 wget "$repolist_in_url" -O /.ignore
 cat /.ignore |sed '/#/d' >/repolist.txt
 #repository list updated to repolist.txt
+
+#file names are stored as txt
+#folder- /datarepository
+cd /
+git clone https://github.com/dev-extended/datarepository
+
+#only reponames for next step
 cat /repolist.txt |sed 's|https://github.com/|prefix180|g' |sed 's/\//\nsuperprefix180/g' |grep 'superprefix180' |sed 's/superprefix180//g' >/reponame.txt
-cat /reponame.txt |sed 's/^/REPONAME="/g' |sed 's/$/"; /g' >env1
-#repo new name env2
-cat /reponame.txt|sed 's/$/ignoreme170/g' |sed 's/-n-/\n/g' |grep 'developeranaz' |sed 's/^/REPONEWNAME="/g' |sed 's/$/-n-$(randomhash)"; /g' >/envnewname
 
-#linkname env3
-cat /reponame.txt|sed 's/$/ignoreme170/g' |sed 's/-n-/\n/g' |grep 'developeranaz' >repoid
-#env4
-echo $datareponame |
+#REPONAME=OLD-REPO >oldreponameequals
+cat /reponame.txt |sed 's/^/REPONAME="/g' |sed 's/$/"; /g' >/envoldreponameequals
 
-#dottxt
-cat /repoid |sed 's/$/.txt/g' >/repolinktxt
-#to data repo 
-# echo "https://$prefix/$newnames" >/datarepo/$datareponame/$repolinktxt; √push
-cat /envnewname | sed 's|^|echo "$PREFIXURL/|g' |sed 's/$/" >\/datarepo\/$datareponame\//g' >/push1
-paste -d'\0' /push1 /repolinktxt >savedottxt
-#paste push1
+#NEWREPONAME= NEW RANDOM REPONAME
+cat /reponame.txt|sed 's/$/ignoreme170/g' |sed 's/-n-/\n/g' |grep 'developeranaz' |sed 's/^/REPONEWNAME="/g' |sed 's/$/-n-$(randomhash)"; /g' >/envnewreponameequals
 
-cat /repolist.txt |sed 's/^/gh repo clone /g' |sed 's/$/; cd \//g' >/s1
-paste -d'\0' /envnewname /env1 /s1 /reponame.txt |sed 's/$/; gh repo rename "$REPONEWNAME" -y; cd \//g' >final1
-## end line +(above )not compl*d
-#not yet coml*ted
+#linkname part for sending to dottxt (only in the format list --not-env) #### has a "developeranaz" identifier
+cat /reponame.txt|sed 's/$/ignoreme170/g' |sed 's/-n-/\n/g' |grep 'developeranaz' >/repoid
+#repoid to env repoid
+cat /repoid |sed 's/^/REPOID="/g' |sed 's/$/"; /g' >/envrepoidequals
+
+##gh bulk cloning part #file ghrnpu + RENAMING +RENAME PUSHING + sending to txt to data repo
+#command - gh repo clone htt....... --
+
+cat /repolist.txt | sed 's/^/gh repo clone "/g' |sed 's/$/";/g' | sed 's/$/ cd "\/$REPONAME"; gh repo rename "$REPONEWNAME"; echo "https:\/\/github.com\/$REPONEWNAME" >\/datarepository\/$REPOID.txt; cd \//g' >/ghrnpu
+
+paste -d'\0' /envoldreponameequals /envnewreponameequals /ghrnpu >/superscript.sh
+cd /
+date > /date
+chmod +x /superscript.sh
+cd /datarepository
+git init 
+git add .
+git commit -m "$(cat /date)"
+git push
