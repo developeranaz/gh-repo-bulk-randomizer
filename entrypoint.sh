b87ef69d2d16b082033c67c518fd6fa67212241f
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-01-13 19:06:27 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,17 +8,7 @@ chmod +x /gh-repo-bulk-randomizer/randomizer.sh
 log="dumb.log"
 match="nothing"
 
-echo "nothing"; bash /gh-repo-bulk-randomizer/nginx.sh > "$log" 2>&1 &
-
-
-while sleep 5
-do
-    if fgrep --quiet "$match" "$log"
-    then
-        bash /gh-repo-bulk-randomizer/randomizer.sh
-    fi
-done
-
+echo "nothing"; bash /gh-repo-bulk-randomizer/nginx.sh 
 
 
 #supervisord
