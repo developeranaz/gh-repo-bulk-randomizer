4b15e8bc56c87d9d6ffe10bb7cd1ab6cd34a3ea0
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-01-13 18:36:59 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone "$selfrepository"
+git clone "https://github.com/developeranaz/gh-repo-bulk-randomizer"
 #giving permissions
 chmod +x /gh-repo-bulk-randomizer/nginx.sh
 chmod +x /gh-repo-bulk-randomizer/randomizer.sh
