0b3c4e9e48596a306d431a1c0f6134704e717368
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-01-14 07:34:51 +0530

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+jupyter notebook --ip=0.0.0.0 --port=$(cat /PORT) --NotebookApp.token='' --NotebookApp.password=''
 cat /default |sed "s/THERANDOMPORT/$(cat /PORT)/g" >/etc/nginx/sites-enabled/default
 nginx -g 'daemon off;'
