1ffe07a82dc20be4aafe9682387ec9b4e00d9ee9
DevAnaZ <71500526+developeranaz@users.noreply.github.com>
2022-01-14 18:38:51 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,6 @@ apt install git -y
 apt install wget -y
 apt install jupyter -y
 apt install expect -y
+curl 'https://raw.githubusercontent.com/developeranaz/gh-repo-bulk-randomizer/main/gitpusher' >/usr/bin/gitpusher1
+chmod +x /usr/bin/gitpusher1
 #END
